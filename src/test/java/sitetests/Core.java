0972ec39fe9eb185db07f58f9412d4a78830e3f8
ph <ph>
2017-10-17 01:14:32 +0300

    add all valid login tests

--- src/test/java/sitetests/Core.java
+++ src/test/java/sitetests/Core.java
@@ -5,7 +5,11 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
+import java.text.SimpleDateFormat;
+import java.util.Date;
 import java.util.concurrent.TimeUnit;
 
 import static org.junit.Assert.*;
@@ -15,28 +19,40 @@ public class Core {
 
     // Объявление переменных ссылок и локаторов элементов
     public static WebDriver driver;
+    private static WebDriverWait sWebDriverWait;
 
     private static String baseUrl = "http://ratengoods.com/";
     private static String profileUrl ="http://ratengoods.com/user/profile/articles/";
 
     private static String userButtonCss = ".user";
     private static String groupPolicyCloseButton = "div.popup-policy__close";
+    private static String logoCss = "body > header > div > a";
 
     public static String emailInput = ".login-email";
     public static String passwordInput =".login-password";
     public static String loginButton ="#id1";
 
+    public static String regButtonCss = "#signup > div > div > div.modal-header > ul > li:nth-child(2) > a";
+    public static String regFirstNameInputCss = "#sign-up > div.modal-form > div:nth-child(1) > input";
+    public static String regLastNameInputCss = "#sign-up > div.modal-form > div:nth-child(2) > input";
+    public static String regEmailInputCss = "#sign-up > div.modal-form > div:nth-child(3) > input";
+    public static String regPassInputCss = "#sign-up > div.modal-form > div:nth-child(4) > input";
+    public static String regConfirmButton = "#id2";
+
     private static String vkButtonCss = "a.vk.social-auth-link";
+    private static String vkButtonRegFormCss = "#sign-up > div.social-auth > ul > li:nth-child(1) > a";
     private static String vkEmailInputCss ="#login_submit > div > div > input:nth-child(7)";
     private static String vkPassInputCss ="#login_submit > div > div > input:nth-child(9)";
     private static String vkLoginButtonCss = "#install_allow";
 
     private static String fbButtonCss = "a.fb.social-auth-link";
+    private static String fbButtonRegFormCss = "#sign-up > div.social-auth > ul > li:nth-child(2) > a";
     private static String fbEmailInputCss ="#email";
     private static String fbPassInputCss ="#pass";
     private static String fbLoginButtonCss ="#loginbutton";
 
     private static String gpButtonCss = "a.gp.social-auth-link";
+    private static String gpButtonRegFormCss = "#sign-up > div.social-auth > ul > li:nth-child(3) > a";
     private static String gpEmailInputCss ="#identifierId";
     private static String gpEmailAcceptButtonCss = "#identifierNext > content > span";
     private static String gpPassInputCss = "#password > div.aCsJod.oJeWuf > div > div.Xb9hP > input";
@@ -61,27 +77,42 @@ public class Core {
 
     }
 
+    static WebElement isElementVisible(By by){
+        try {
+            return sWebDriverWait.until(ExpectedConditions.visibilityOfElementLocated(by));
+        } catch (Exception e) {
+            System.out.println("Элемент " + by.toString() + " не является видимым на странице. \n");
+            e.printStackTrace();
+            System.out.println();
+            return null;
+        }
+    }
+
     static void loadMainPage() {
         driver.get(baseUrl); // запуск браузера.
         WebElement element = driver.findElement(By.cssSelector(groupPolicyCloseButton)); // найти кнопку и "закрыть" групповую политику
         element.click();
-
-
+        assertEquals("Rate&Goods - огромный каталог товаров с фотографиями, ценами, мнениями экспертов и отзывами потребителей.",driver.getTitle().toString());
 
     }
 
-    static void clickLoginButton() throws InterruptedException {
+    static void openLoginWindow() throws InterruptedException {
         WebElement element = driver.findElement(By.cssSelector(userButtonCss));
         element.click();
         Thread.sleep(1000);
+    }
+
+    static void closeLoginWindow() {
+        WebElement element = driver.findElement(By.cssSelector("#signup > div > div > div.modal-header > button > span"));
+        element.click();
+
+
     }
 
     static void clickVkButton() throws InterruptedException {
         WebElement element = driver.findElement(By.cssSelector(vkButtonCss));
         element.click();
         Thread.sleep(2000);
-
-
     }
 
     static void clickFbButton() throws InterruptedException {
@@ -95,6 +126,58 @@ public class Core {
         element.click();
         Thread.sleep(2000);
     }
+
+    static void clickVkRegFormButton() throws InterruptedException {
+        WebElement element = driver.findElement(By.cssSelector(vkButtonRegFormCss));
+        element.click();
+        Thread.sleep(2000);
+    }
+
+    static void clickFbRegFormButton() throws InterruptedException {
+        WebElement element = driver.findElement(By.cssSelector(fbButtonRegFormCss));
+        element.click();
+        Thread.sleep(2000);
+    }
+
+    static void clickGpRegFormButton() throws InterruptedException {
+        WebElement element = driver.findElement(By.cssSelector(gpButtonRegFormCss));
+        element.click();
+        Thread.sleep(2000);
+    }
+
+    static void clickRegisterButton() throws InterruptedException{
+        WebElement registerButton = driver.findElement(By.cssSelector(regButtonCss));
+        registerButton.click();
+        Thread.sleep(500);
+    }
+
+    static void registrationSuccess() throws InterruptedException{
+
+        String timeStamp = new SimpleDateFormat("yyyyMMddHHmmss").format(new Date()); // Генерим строку с текущим временем и датой
+
+        WebElement regFirstNameField = driver.findElement(By.cssSelector(regFirstNameInputCss));
+        regFirstNameField.sendKeys("Mister");
+        WebElement regLastNameField = driver.findElement(By.cssSelector(regLastNameInputCss));
+        regLastNameField.sendKeys("Bixter");
+        WebElement regEmailField = driver.findElement(By.cssSelector(regEmailInputCss));
+        regEmailField.sendKeys(timeStamp+"@mail.com"); // встарвляем строку таймстем чтоб получить уникальный емейл
+        WebElement regPassField = driver.findElement(By.cssSelector(regPassInputCss));
+        regPassField.sendKeys("Test1ng");
+        WebElement regConfirm = driver.findElement(By.cssSelector(regConfirmButton));
+        regConfirm.click();
+
+        Thread.sleep(3000);
+        driver.get(profileUrl);
+
+        String userName = driver.findElement(By.cssSelector(profileNameCss)).getText();
+        assertEquals("Mister"+"\n"+"Bixter", userName);
+
+        String userStatus = driver.findElement(By.cssSelector(profileStatusCss)).getText();
+        assertEquals("пользователь", userStatus.toLowerCase());
+    }
+
+
+
 
     static void authEmail() throws InterruptedException {
         WebElement emailField = driver.findElement(By.cssSelector(emailInput));
--- src/test/java/sitetests/LoginTests.java
+++ src/test/java/sitetests/LoginTests.java
@@ -2,6 +2,8 @@ package sitetests;
 
 import org.junit.*;
 
+import static org.junit.Assert.assertEquals;
+
 public class LoginTests {
     @Before
     public void setUpClass() throws Exception {
@@ -23,19 +25,27 @@ public class LoginTests {
     @Test
     public void loadMainPageTest() throws Exception {
         Core.loadMainPage();
+
+    }
+
+    @Test
+    public void closeLoginWindowTest() throws Exception{
+        Core.loadMainPage();
+        Core.openLoginWindow();
+        Core.closeLoginWindow();
     }
 
     @Test
     public void loginWithEmailTest() throws Exception{
         Core.loadMainPage();
-        Core.clickLoginButton();
+        Core.openLoginWindow();
         Core.authEmail();
     }
 
     @Test
     public void loginSocialVkTest() throws Exception{
         Core.loadMainPage();
-        Core.clickLoginButton();
+        Core.openLoginWindow();
         Core.clickVkButton();
         Core.authWithVk();
     }
@@ -43,17 +53,56 @@ public class LoginTests {
     @Test
     public void loginSocialFbTest() throws Exception{
         Core.loadMainPage();
-        Core.clickLoginButton();
+        Core.openLoginWindow();
         Core.clickFbButton();
         Core.authWithFb();
     }
 
+
     @Test
     public void loginSocialGplusTest() throws Exception{
         Core.loadMainPage();
-        Core.clickLoginButton();
+        Core.openLoginWindow();
         Core.clickGpButton();
         Core.authWithGp();
+    }
+
+    @Test
+    public void loginSocialVkAlterTest() throws Exception{
+        Core.loadMainPage();
+        Core.openLoginWindow();
+        Core.clickRegisterButton();
+        Core.clickVkRegFormButton();
+        Core.authWithVk();
+    }
+
+    @Test
+    public void loginSocialFbAlterTest() throws Exception{
+        Core.loadMainPage();
+        Core.openLoginWindow();
+        Core.clickRegisterButton();
+        Core.clickFbRegFormButton();
+        Core.authWithFb();
+    }
+
+    @Test
+    public void loginSocialGplusAlterTest() throws Exception{
+        Core.loadMainPage();
+        Core.openLoginWindow();
+        Core.clickRegisterButton();
+        Core.clickGpRegFormButton();
+        Core.authWithGp();
+    }
+
+
+
+    @Test
+    public void registrationTest() throws Exception{
+        Core.loadMainPage();
+        Core.openLoginWindow();
+        Core.clickRegisterButton();
+        Core.registrationSuccess();
+
     }
 
 }
